2b8ba1f38914bafc575557f1751445489a3fdfa0
Marcos Vinicius Souza Campos <marcos.campos.ext@conductor.com.br>
2019-12-10 16:06:44 -0300

    Adicionando efeito de transição no menu

--- src/components/menu-item/MenuItem.component.jsx
+++ src/components/menu-item/MenuItem.component.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 import "./MenuItem.styles.scss";
 
 const MenuItem = ({ title, imageUrl, size }) => (
-    <div style={{
-        backgroundImage: `url(${imageUrl})`
-    }} className={`menu-item ${(size ? size : '')}`}>
+    <div className={`menu-item ${(size ? size : '')}`}>
+        <div className="background-image" style={{
+            backgroundImage: `url(${imageUrl})`
+        }}/>
         <div className="content">
             <h1 className="title">{title}</h1>
             <span className="subtitle">Shop Now</span>
