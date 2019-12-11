af725dafff054cc27f1f68559897f6dd30bbe678
Marcos Vinicius Souza Campos <marcos.campos.ext@conductor.com.br>
2019-12-11 15:50:13 -0300

    Configuracao inicial de rotas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+
 import './App.css';
 
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/Homepage.component';
+
+const HatsPage = () => (
+  <div>Hats Page</div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage}/>
+        <Route path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
