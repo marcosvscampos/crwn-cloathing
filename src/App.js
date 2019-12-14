3900a5319a1b87481a5cfcd2e1baf80c8089dc7e
Marcos Vinicius Souza Campos <marcos.campos.ext@conductor.com.br>
2019-12-14 18:03:34 -0300

    Adição de elementos no header

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/Homepage.container';
 import ShopPage from './pages/shop/Shop.container';
+import Header from './components/header/Header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage}/>
         <Route path="/shop" component={ShopPage} />
