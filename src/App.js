17c2210ff458510e25fae818d3d192e375f9fa73
Marcos Vinicius Souza Campos <marcos.campos.ext@conductor.com.br>
2019-12-25 08:53:52 -0300

    Usando constantes para actions de usuário

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import './App.css';
 
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import HomePage from './pages/homepage/Homepage.container';
 import ShopPage from './pages/shop/Shop.container';
 import Header from './components/header/Header.component';
@@ -18,17 +18,17 @@ class App extends Component {
 
   componentDidMount() {
     const { setCurrentUser } = this.props;
-    
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if(userAuth){
+      if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapshot => {
           setCurrentUser({
-              id : snapshot.id,
-              ...snapshot.data()
+            id: snapshot.id,
+            ...snapshot.data()
           });
-        }) 
+        })
       }
       setCurrentUser(userAuth)
     })
@@ -41,18 +41,26 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header/>
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInSignUp} />
+          <Route path="/signin" render={() => this.props.currentUser ? (
+              <Redirect to="/" />
+            ) : (
+              <SignInSignUp />
+            )} />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
